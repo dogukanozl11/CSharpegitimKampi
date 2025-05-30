1e3af696073b09a0b30c9cdf1af1e1d5ea8ff36c
Doğukan ÖZEL <dogukanozel692@gmail.com>
2025-05-30 18:44:00 +0300

    2.Ders Sonu

--- 02_Variables/Program.cs
+++ 02_Variables/Program.cs
@@ -80,30 +80,92 @@ namespace _02_Variables
 
             #region Klavyeden Veri Girişleri String Değişkenler
 
-            Console.WriteLine("**** CSharp Hava Yolları Yolcu Bilgisi ****");
-            Console.WriteLine();
+            //Console.WriteLine("**** CSharp Hava Yolları Yolcu Bilgisi ****");
+            //Console.WriteLine();
 
-            string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge, passengerIdentityNumber;
+            //string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge, passengerIdentityNumber;
 
-            Console.Write("Yolcu Adı : ");
-            passengerName = Console.ReadLine();
-            Console.Write("Yolcu Soyadı : ");
-            passengerSurname = Console.ReadLine();
-            Console.Write("İlçe Bilgisi : ");
-            passengerDistrict = Console.ReadLine();
-            Console.Write("İl bilgisi : ");
-            passengerCity = Console.ReadLine();
-            Console.Write("Yaş : ");
-            passengerAge = Console.ReadLine();
-            Console.Write("TC Kimlik Numarası : ");
-            passengerIdentityNumber = Console.ReadLine();
+            //Console.Write("Yolcu Adı : ");
+            //passengerName = Console.ReadLine();
+            //Console.Write("Yolcu Soyadı : ");
+            //passengerSurname = Console.ReadLine();
+            //Console.Write("İlçe Bilgisi : ");
+            //passengerDistrict = Console.ReadLine();
+            //Console.Write("İl bilgisi : ");
+            //passengerCity = Console.ReadLine();
+            //Console.Write("Yaş : ");
+            //passengerAge = Console.ReadLine();
+            //Console.Write("TC Kimlik Numarası : ");
+            //passengerIdentityNumber = Console.ReadLine();
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Console.WriteLine("*-----------------------------------------------*");
-            Console.WriteLine("Yolcu : " + passengerName + " " + passengerSurname);
+            //Console.WriteLine("*-----------------------------------------------*");
+            //Console.WriteLine("Yolcu TC kimlik No  : " + passengerIdentityNumber + " - Yolcu Ad Soyad : " + passengerName+ " " + passengerSurname + " " + passengerDistrict + " / "+ passengerCity + " " +passengerAge);
+
 
             #endregion
+
+            #region Klavyeden Tam Sayı Girişleri ve Dönüşümler
+
+            //int shoesPrice, computerPrice, chairPrice, tvPrice;
+
+            //shoesPrice = 1000;
+            //computerPrice = 20000;
+            //chairPrice = 5000;
+            //tvPrice = 12000;
+
+            //int shoesCount, computerCount, chairCount, tvCount;
+
+            //Console.Write("Lütfen aldığınız ayakkabı sayısını giriniz : ");
+            //shoesCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Lütfen aldığınız bilgisayar sayısını giriniz : ");
+            //computerCount = int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen aldığınız sandalye sayısını giriniz : ");
+            //chairCount = int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen aldığınız televizyon sayısını giriniz : ");
+            //tvCount = int.Parse(Console.ReadLine());
+
+            //int totalPrice=shoesCount*shoesPrice+computerCount*computerPrice+chairCount*chairPrice+tvCount*tvPrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam Ödemeniz Gereken : " + totalPrice + " TL ");
+            #endregion
+
+            #region Klavyeden Ondalık Sayı İşlemleri
+
+            //double exam1, exam2, exam3, result;
+
+            //Console.Write( "Lütfen 1.Sınav notunu giriniz : " );
+            //exam1 = double.Parse( Console.ReadLine() );
+
+
+            //Console.Write("Lütfen 2.Sınav notunu giriniz : ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+
+            //Console.Write("Lütfen 3.Sınav notunu giriniz : ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+            //result = (exam1+exam2+exam3) / 3;
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Sınav Ortalamanız : " + result);
+
+            #endregion
+
+            #region Klavyeden Karakter Girişleri
+
+            //char gender;
+            //Console.Write("Lütfen cinsiyet seçiniz : ");
+            //gender=char.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Seçtiğiniz Cinsiyet : " + gender );
+
+            #endregion
+
             Console.Read();
         }
     }
